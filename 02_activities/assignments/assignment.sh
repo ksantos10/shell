e47a65e1af607dc93f92dcb5beaaf3abaff9d07c
Santos <kimlee.santos@ised-isde.gc.ca>
2026-07-28 12:04:06 -0400

    fix path typo: .data/raw -> ./data/raw

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -32,10 +32,10 @@ unzip -q rawdata.zip
 mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
-mv ./rawdata .data/raw
+mv ./rawdata ./data/raw #missing a "/" added
 
 # 3. List the contents of the ./data/raw directory
-ls .data/raw
+ls ./data/raw #missing a "/" added
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 mkdir ./data/processed
